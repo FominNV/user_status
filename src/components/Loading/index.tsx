f95939a32c4c8f7f489f288bb4ab57df575f751e
Nikolai Fomin <kk309@mail.ru>
2022-04-26 16:23:07 +0300

    create Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import ReactLoading from "react-loading";
+
+import "./styles.scss";
+
+const Loading: FC = () => (
+  <div className="Loading">
+    <ReactLoading
+      type="spinningBubbles"
+      color="green"
+      height={36}
+      width={36}
+    />
+  </div>
+);
+
+export default Loading;
